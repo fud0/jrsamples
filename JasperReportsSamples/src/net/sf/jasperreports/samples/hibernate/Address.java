70ab1cdaa07138bd2cbdf35430cb024193346c86
Massimo Rabbi <mrabbi@tibco.com>
2020-02-18 15:34:29 +0100

    Fixed Hibernate sample.

--- JasperReportsSamples/src/net/sf/jasperreports/samples/hibernate/Address.java
+++ JasperReportsSamples/src/net/sf/jasperreports/samples/hibernate/Address.java
@@ -1,6 +1,6 @@
 /*
  * JasperReports - Free Java Reporting Library.
- * Copyright (C) 2001 - 2014 TIBCO Software Inc. All rights reserved.
+ * Copyright (C) 2001 - 2019 TIBCO Software Inc. All rights reserved.
  * http://www.jaspersoft.com
  *
  * Unless you have purchased a commercial license agreement from Jaspersoft,
@@ -102,6 +102,7 @@ public class Address
 		this.documents = documents;
 	}
 
+	@Override
 	public String toString()
 	{
 		return id + ":" + firstName + "," + lastName + "," + street + "," + city;
